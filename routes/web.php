4320ef235e057c8210d7eb3276dd9bdfc48d3d43
Nouman Shoaib <nouman.shoaib786@gmail.com>
2018-05-20 15:44:28 +0500

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routess
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
